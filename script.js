2e6a15f39790b562ce0ad0ffc66d8c9cea714426
Bobby Willmes <bobbywillmes@gmail.com>
2021-03-03 08:42:53 -0800

    app component

--- script.js
+++ script.js
@@ -7,9 +7,11 @@ var element = React.createElement(
 var Temperature = function Temperature(props) {
   console.log('props: ', props);
   return React.createElement(
-    "h1",
+    "p",
     null,
-    "The current temperature is ",
+    "The current temperature in ",
+    props.city,
+    " is ",
     props.degree,
     " degree ",
     props.unit
@@ -18,6 +20,17 @@ var Temperature = function Temperature(props) {
 
 var tempElement = React.createElement(Temperature, { degree: 25, unit: "celsius" });
 
+var App = function App() {
+  return React.createElement(
+    "div",
+    null,
+    React.createElement(Temperature, { city: "London", degree: 20, unit: "celsius" }),
+    React.createElement(Temperature, { city: "New York", degree: 25, unit: "celsius" }),
+    React.createElement(Temperature, { city: "Dubai", degree: 32, unit: "celsius" })
+  );
+};
+
 ReactDOM.render(
 // element,
-tempElement, document.getElementById('root'));
+// tempElement,
+React.createElement(App, null), document.getElementById('root'));
--- src/script.js
+++ src/script.js
@@ -2,13 +2,24 @@ const element = <h1 className="greeting">Hello world!</h1>
 
 const Temperature = (props) => {
   console.log('props: ', props);
-  return <h1>The current temperature is {props.degree} degree {props.unit}</h1>;
+  return <p>The current temperature in {props.city} is {props.degree} degree {props.unit}</p>;
 };
 
 const tempElement = <Temperature degree={25} unit="celsius" />;
 
+const App = () => {
+  return (
+    <div>
+      <Temperature city="London" degree={20} unit="celsius" />
+      <Temperature city="New York" degree={25} unit="celsius" />
+      <Temperature city="Dubai" degree={32} unit="celsius" />
+    </div>
+  );
+}
+
 ReactDOM.render(
   // element,
-  tempElement,
+  // tempElement,
+  <App />,
   document.getElementById('root')
 );
